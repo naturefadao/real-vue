c95b1873514b039ddf88c3270de2aa3e9ed89f6b
naturefadao <2508763206@qq.com>
2024-09-03 16:22:21 +0800

    first card without data in parent view

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,21 +1,43 @@
 <script setup>
+import { ref } from 'vue'
 // defineProps({
 //   msg: {
 //     type: String,
 //     required: true
 //   }
 // })
+const event = ref({
+  id: 5928101,
+  category: 'animal welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event.',
+  location: 'Meow Town',
+  date: 'January 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
-  <div class="greetings">
-    <!-- <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3> -->
+  <div class="event-card">
+    <!-- display event data -->
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.data }}</span>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from '@/components/EventCard.vue'
+//这是父主视图
+/* '@/components/EventCard.vue'等同于'../components/EventCard.vue'
+@ 是一种常用的别名，用于简化模块导入路径。这个别名通常在项目的配置文件中定义，例如 Webpack、Vite 或其他构建工具的配置。
+@ 符号通常代表项目的根目录或者 src 目录。这样写可以让你在导入模块时避免使用相对路径，这样可以使导入路径更加简洁和可读。
+*/
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <EventCard />
+    <!-- ES6 模块导入语法 -->
+  </div>
 </template>
